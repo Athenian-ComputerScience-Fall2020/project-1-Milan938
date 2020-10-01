279546da7b993e5a994f40171527dc7dec5cff70
Milan938 <23mboga@athenian.org>
2020-10-01 17:05:29 +0000

    Not done yet

--- my_code.py
+++ my_code.py
@@ -1,5 +1,8 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  None so far
+
+def pres_info(correct_answer, hint_1, hint_2, hint_3, hint_4, hint_5):
+
 
 
 
